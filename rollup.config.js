c32825b3c596b64dd30b43e25c0bbe34a2d258b1
ziponia <kuby@amayzer.com>
2021-08-25 23:59:41 +0900

    config rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,14 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default {
   input: "./index.ts",
   output: {
-    file: "./dist/index.js",
-    name: "iamport",
+    file: pkg.main,
+    name: pkg.name,
+    format: "cjs",
   },
   plugins: [
     typescript({
